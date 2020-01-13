ca0535fc20925b137046bad142e9a4a1f5c6975d
munich99 <regerstrasse@icloud.com>
2020-01-13 20:09:12 +0100

    routing - secure route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './admin/admin.component'
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from './auth.guard'
 
 
 const routes: Routes = [
   {path:'', pathMatch: 'full', redirectTo: 'login'},
   {path:'login', component: LoginComponent},
-  {path:"admin", component: AdminComponent}
+  {path:"admin", component: AdminComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
